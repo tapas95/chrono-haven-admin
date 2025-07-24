95c0c4f889bbbc477757f2258e2e24b9a4663cae
Tapas <tapas.paul@codeclouds.in>
2025-07-24 17:41:16 +0530

    Fix Design issue in the Login Page

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -4,7 +4,7 @@ import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 
 const InputText = props => {
-    const defaultInputFieldClass = `${ props.type === 'password' ? 'pl-4 pr-12' : 'px-4' } py-3 w-full border border-solid border-gray-300 outline-none rounded-md`;
+    const defaultInputFieldClass = `${ props.type === 'password' ? 'pl-4 pr-12' : 'px-4' } text-sm py-3 w-full border border-solid border-gray-300 outline-none rounded-md`;
     let [ passwordIsVisible, setPasswordIsVisible ] = useState( false );
     const handlePasswordVisibility = () =>  setPasswordIsVisible( passwordIsVisible = !passwordIsVisible );
     if( props.type === 'password' ){
